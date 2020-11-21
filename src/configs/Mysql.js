5ab69197f500beb5011a60f2dcc7a2a1c0d2cdc6
Salaki na Komaki <komaki4lyfe@gmail.com>
2020-11-21 14:56:07 +0700

    fix database initialization

--- src/configs/Mysql.js
+++ src/configs/Mysql.js
@@ -22,29 +22,29 @@ function createDb(config) {
     }
 }
 
-function initDb() {
+async function initDb() {
     _db = createDb({
         host: MYSQL_HOST,
         user: MYSQL_USER,
-        password: MYSQL_PASS,
-        database: MYSQL_DB
+        password: MYSQL_PASS
     });
-    
-    _db.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}`).then(res => {
-        _db.query(`
-            CREATE TABLE IF NOT EXISTS user (
-                id int AUTO_INCREMENT PRIMARY KEY,
-                username varchar(255),
-                password varchar(255)
-            )
-            SELECT 'admin' AS \`username\`, 'seblak' AS \`password\`
-        `).then(ress => {
-            if (ress.warningCount == 0) {
-                console.log(`Table ${MYSQL_TABLE} does not exists, creating tables`);
-                console.log('Creating user with username "admin" and password "seblak"')
-            }
-        })
-    })
+
+    const databaseRequest = await _db.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}`);
+    if (databaseRequest.warningCount == 0) console.log(`Database does not exists, creating database with name ${MYSQL_DB}`);
+    await _db.query(`USE ${MYSQL_DB}`);
+
+    const tableRequest = await _db.query(`
+        CREATE TABLE IF NOT EXISTS ${MYSQL_TABLE} (
+            id int AUTO_INCREMENT PRIMARY KEY,
+            username varchar(255),
+            password varchar(255)
+        )
+        SELECT 'admin' AS \`username\`, 'seblak' AS \`password\`
+    `);
+    if (tableRequest.warningCount == 0) {
+        console.log(`Table does not exists, creating table with name ${MYSQL_TABLE}`);
+        console.log(`Creating user with username "admin" and password "seblak"`);
+    }
 }
 
 module.exports = {
